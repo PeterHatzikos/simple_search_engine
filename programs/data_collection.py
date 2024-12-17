9358647998752a0c2ac898d71b4fd36c38f5c887
PeterHatzikos <hatzikospeter@gmail.com>
2024-12-17 18:55:36 +0200

    Update data_collection.py
    changed greek comments into english

--- programs/data_collection.py
+++ programs/data_collection.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Nov  2 17:22:07 2024
-
-@author: Simos
 """
 
 import requests
@@ -10,9 +8,9 @@ from bs4 import BeautifulSoup
 
 def collect_wikipedia_data():
     data_intake = []
-    wiki_texts = {}  # Λεξικό για την αποθήκευση των δεδομένων
+    wiki_texts = {}  # dictionary for storing data
 
-    # Προσθέτω συνδέσμους στη λίστα data_intake
+    # inserting links into list named data_intake
     data_intake.append(requests.get('https://en.wikipedia.org/wiki/Watermelon'))
     data_intake.append(requests.get('https://en.wikipedia.org/wiki/Information_extraction'))
     data_intake.append(requests.get('https://en.wikipedia.org/wiki/Adam_Sandler'))
@@ -20,16 +18,16 @@ def collect_wikipedia_data():
     data_intake.append(requests.get('https://en.wikipedia.org/wiki/Animal_Farm'))
     
 
-    # Επεξεργασία του HTML για την εξαγωγή του κειμένου
+    # processing the HTML to extract the needed texts
     for i, data in enumerate(data_intake):
         soup = BeautifulSoup(data.text, "html.parser")
-        content = ""  # Αρχικοποίηση περιεχομένου
-        for paragraph in soup.select('p'):
-            for sup_search in paragraph.select('sup'): # οπου υπάρχει η λεζάντα " sup " 
-                sup_search.decompose() # αφαίρεσε τη και μην διαβάσεις τα περιεχόμενα της
-            content += paragraph.get_text()  # Εξαγωγή κειμένου
+        content = ""  # initialisation of contents
+        for paragraph in soup.select('p'): # wherever there is the caption <p>
+            for sup_search in paragraph.select('sup'): # wherever there is the caption" sup " 
+                sup_search.decompose() #  remove it and dont read its contents
+            content += paragraph.get_text()  # extracting texts
 
-        # Αποθήκευση του περιεχομένου στο λεξικό
+        # saving contents into dictionary
         wiki_texts[i] = {"title": f"Page {i + 1}", "content": content}
 
     return wiki_texts
